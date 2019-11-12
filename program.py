12c0994b1bd5fbb98019d1a82266b77932e6f487
HEC <EMail@example.ru>
2019-11-12 17:32:18 +1000

    Some new prints to understand how is it works

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
 def main():
     print('My first git program')
     print("And I'm tring to change it everyday")
+    print('fuck')
+    print('this shit')
+    print('i"m out')
 
 
 if __name__ == '__main__':
