5c0d5ecc7991b84a4727187ec4904094208e4a36
HEC <EMail@example.ru>
2019-11-12 17:39:21 +1000

    First file

--- program.py
+++ program.py
@@ -1,10 +1,1 @@
-def main():
-    print('My first git program')
-    print("And I'm tring to change it everyday")
-    print('fuck')
-    print('this shit')
-    print('i"m out')
-
-
-if __name__ == '__main__':
-    main()
+print('My first git-repo')
